e3c7d668cfad1df84793dfdf2cd39c634c6c9392
Austin Maung <technicoal@gmail.com>
2021-02-22 13:49:57 -0800

    working on deletion of fraction

--- app.js
+++ app.js
@@ -42,4 +42,30 @@ function dragElement(elmnt) {
       document.onmousemove = null;
    }
 }
+document.querySelector('div').addEventListener('keydown', function(event) {
+   // Check for a backspace
+   console.log('test')
+   if (event.which == 8) {
+      console.log(';asd')
+       s = window.getSelection();
+       r = s.getRangeAt(0)
+       el = r.startContainer.parentElement
+       // Check if the current element is the .label
+       if (el.classList.contains('frac')) {
+           // Check if we are exactly at the end of the .label element
+           if (r.startOffset == r.endOffset && r.endOffset == el.textContent.length) {
+               // prevent the default delete behavior
+               event.preventDefault();
+               if (el.classList.contains('highlight')) {
+                   // remove the element
+                   el.remove();
+               } else {
+                   el.classList.add('highlight');
+               }
+               return;
+           }
+       }
+   }
+   event.target.querySelectorAll('span.frac.highlight').forEach(function(el) { el.classList.remove('highlight');})
+});
 
