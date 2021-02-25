aff2e4c87cbf45bb66ed39dace935ff9d1d17520
Austin Maung <tecknicoal@gmail.com>
2021-02-24 18:38:28 -0800

    added way to delete fractions, nested no

--- app.js
+++ app.js
@@ -44,27 +44,41 @@ function dragElement(elmnt) {
 }
 document.querySelector('div').addEventListener('keydown', function(event) {
    // Check for a backspace
-   console.log('test')
+   console.log("checking for backspace")
    if (event.which == 8) {
-      console.log(';asd')
-       s = window.getSelection();
-       r = s.getRangeAt(0)
-       el = r.startContainer.parentElement
-       // Check if the current element is the .label
-       if (el.classList.contains('frac')) {
-           // Check if we are exactly at the end of the .label element
-           if (r.startOffset == r.endOffset && r.endOffset == el.textContent.length) {
-               // prevent the default delete behavior
-               event.preventDefault();
-               if (el.classList.contains('highlight')) {
-                   // remove the element
-                   el.remove();
-               } else {
-                   el.classList.add('highlight');
-               }
-               return;
-           }
-       }
+      console.log('yes backspace')
+      s = window.getSelection();
+      console.log(s)
+      r = s.getRangeAt(0)
+      console.log(r)
+      console.log(r.startContainer.parentElement.parentElement)
+      if(r.startContainer.parentElement.classList.contains('numer') || r.startContainer.parentElement.classList.contains('denom')){
+         el = r.startContainer.parentElement.parentElement
+      }
+      else if(r.startContainer.parentElement.classList.contains('delete')){
+         el = r.startContainer.parentElement.previousSibling
+      }
+
+      
+      console.log(el)
+      // console.log(child)
+      // Check if the current element is the .label
+      if (el.classList.contains('frac')) {
+         console.log("in fraction")
+         // Check if we are exactly at the end of the .label element
+         if ((r.startOffset == r.endOffset && r.endOffset == 0) || (r.startContainer.parentElement.classList.contains('delete') && r.endOffset == 1)) {
+            console.log("at the end of fraction")
+            // prevent the default delete behavior
+            event.preventDefault();
+            if (el.classList.contains('highlight')) {
+               // remove the element
+               el.remove();
+            } else {
+               el.classList.add('highlight');
+            }
+            return;
+         }
+      }
    }
    event.target.querySelectorAll('span.frac.highlight').forEach(function(el) { el.classList.remove('highlight');})
 });
